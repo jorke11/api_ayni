1ae60ee9a43402461e2eb3a879ba66d42466a37c
Jorge pinedo <jpinedom@hotmail.com>
2026-08-13 04:23:27 -0500

    change ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: "api-wyni-x2",
-      script: "node_modules/.bin/next",
-      args: "start",
+      name: "api-ayni",
+      script: "dist/main.js",
       cwd: "/home/webapp/projects/ayni/api_ayni",
       instances: 1,
       autorestart: true,
