15c4179b26ae0d2b958719af4178a91f95fb11d9
Jorge pinedo <jpinedom@hotmail.com>
2026-08-13 04:27:50 -0500

    change route

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: "api-ayni",
-      script: "dist/main.js",
-      cwd: "/home/webapp/projects/ayni/api_ayni",
+      name: 'api-ayni',
+      script: 'dist/src/main.js',
+      cwd: '/home/webapp/projects/ayni/api_ayni',
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: "1G",
+      max_memory_restart: '1G',
       env: {
-        NODE_ENV: "production",
+        NODE_ENV: 'production',
         PORT: 8008,
       },
       env_production: {
-        NODE_ENV: "production",
+        NODE_ENV: 'production',
         PORT: 8008,
       },
     },
